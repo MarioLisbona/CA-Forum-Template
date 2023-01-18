2097a80de13b3893d01094384a3cdd5ca4bcd207
Mario Lisbona <mario.lisbona@gmail.com>
2023-01-18 15:58:02 +1100

    Replace bottom nav with footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<>
+			<footer class="footer-container">
+				<section class="d-flex justify-content-start">
+						<div class="p-2 ms-3 mb-3">Socials</div>
+						<div class="p-2 ms-3 mb-3">Copyright</div>
+						<div class="p-2 ms-3 mb-3">T's & C's</div>
+				</section>
+			</footer>
+		</>
+	)
+}
+
+export default Footer
